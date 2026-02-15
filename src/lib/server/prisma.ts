fce25178fbf02daf10e46794f67ea96acdee1aed
Lorenzo Tesler-Mabe <lorenzoteslermabe@gmail.com>
2026-02-14 21:59:56 -0800

    Configure Prisma with Better SQLite3 adapter and dynamic database URL

---
+++ src/lib/server/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { env } from '$env/dynamic/private';
+
+const url = env.DATABASE_URL ?? 'file:./dev.db';
+
+const adapter = new PrismaBetterSqlite3({ url });
+
+export const prisma = new PrismaClient({ adapter });
