ed3754a6daa5abf0f668984d5e9d3c83cf275d30
Lorenzo Tesler-Mabe <lorenzoteslermabe@gmail.com>
2026-03-14 02:30:52 -0700

    Update theme and background color to black in PWA manifest and global styles

--- vite.config.ts
+++ vite.config.ts
@@ -4,31 +4,31 @@ import { defineConfig } from 'vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    sveltekit(),
-    SvelteKitPWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Woof Watch',
-        short_name: 'Woof',
-        display: 'standalone',
-        start_url: '/',
-        theme_color: '#0f172a',
-        background_color: '#0f172a',
-        icons: [
-          {
-            src: 'images/pwa-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'images/pwa-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-    })
-  ]
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Woof Watch',
+				short_name: 'Woof',
+				display: 'standalone',
+				start_url: '/',
+				theme_color: '#000000',
+				background_color: '#000000',
+				icons: [
+					{
+						src: 'images/pwa-192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'images/pwa-512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		})
+	]
 });
